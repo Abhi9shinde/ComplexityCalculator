1ada1a07cb498fe01c13a6a4432a911dfdf02987
Abhinav Shinde <143414991+Abhi9shinde@users.noreply.github.com>
2025-02-06 00:45:18 +0530

    testing1

---
+++ api/proxy.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  const url = import.meta.env.VITE_API_URL;
+  const externalApiUrl = url;
+
+  try {
+    const response = await fetch(externalApiUrl, {
+      method: req.method,
+      headers: req.headers,
+      body: req.method === "POST" ? req.body : undefined,
+    });
+
+    const data = await response.json();
+
+    res.status(response.status).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch data from the API" });
+  }
+}
